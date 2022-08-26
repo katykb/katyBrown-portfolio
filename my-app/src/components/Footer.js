50f5b8bb1cf10c42a2a3d05563c841fded47e35d
Katy Brown <katybr35@gmail.com>
2022-08-25 23:44:50 -0500

    added links and copyright line to footer

---
+++ my-app/src/components/Footer.js
@@ -0,0 +1,75 @@
+import React from "react";
+import "./FooterStyle.css";
+import {
+  FaHome,
+  FaPhone,
+  FaMailBulk,
+  FaGithub,
+  FaLinkedin,
+} from "react-icons/fa";
+
+export const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="footer-container">
+        <div className="left">
+          <div className="location">
+            <FaHome size={30} style={{ color: "#fff", marginRight: "2rem" }} />
+            <div>
+              <p>Based in Dallas/Fort Worth area</p>
+            </div>
+          </div>
+          <div className="phone">
+            <h4>
+              <FaPhone
+                size={30}
+                style={{ color: "#fff", marginRight: "2rem" }}
+              />
+              817-454-3409
+            </h4>
+          </div>
+          <div className="email">
+            <h4>
+              <FaMailBulk
+                size={30}
+                style={{ color: "#fff", marginRight: "2rem" }}
+              />
+              katybr35@gmail.com
+            </h4>
+          </div>
+        </div>
+        <div className="right">
+          <h4>About</h4>
+          <p>
+            Front end: HTML5, CSS, JavaScript, React, Bootstrap <br />
+            Back end: Node.js, Express.js, APIs, MongoDB, SQL <br /> Control
+            Systems: Git, Git Hub
+          </p>
+          <div className="social">
+            <a id="social-icon" href="https://github.com/katykb">
+              <FaGithub
+                size={30}
+                style={{ color: "#fff", marginRight: "2rem" }}
+              />
+            </a>
+            <a
+              id="social-icon"
+              href="https://www.linkedin.com/in/katy-b-a7984084/"
+            >
+              <FaLinkedin
+                size={30}
+                style={{ color: "#fff", marginRight: "2rem" }}
+              />
+            </a>
+          </div>
+        </div>
+        <div className="copyright">
+          {" "}
+          &copy;{new Date().getFullYear()} | Katy Brown{" "}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
--- my-app/src/routes/Home.js
+++ my-app/src/routes/Home.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from '../components/Navbar';
 import Hero from "../components/Hero";
+import Footer from "../components/Footer";
 
 
 export const Home = () => {
@@ -8,6 +9,7 @@ export const Home = () => {
     <div> 
       <Navbar />
       <Hero />
+      <Footer />
     </div>
   )
 }
