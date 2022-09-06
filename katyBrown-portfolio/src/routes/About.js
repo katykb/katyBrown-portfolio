832d8920c61f520f7013977e7c78e4ddbea39a37
Katy Brown <katybr35@gmail.com>
2022-09-06 11:02:31 -0500

    changed app name

---
+++ katyBrown-portfolio/src/routes/About.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Navbar from '../components/Navbar';
+import Footer from "../components/Footer";
+import Hero2 from "../components/Hero2"
+import AboutContent from '../components/AboutContent';
+
+const About = () => {
+  return (
+    <div>
+      <Navbar />
+      <Hero2 heading="ABOUT" />
+      <AboutContent />
+      <Footer />
+    </div>
+  )
+}
+
+export default About
---
+++ katyBrown-portfolio/src/routes/Projects.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import Hero2 from "../components/Hero2"
+import Work from "../components/Work";
+
+const Projects = () => {
+  return (
+    <div>
+      <Navbar />
+      <Hero2 heading="PROJECTS" />
+      <Work/>
+      <Footer />
+    </div>
+  );
+};
+
+export default Projects;
